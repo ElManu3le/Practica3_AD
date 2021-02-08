5c58904eb3312620c837889815a08acdd021379c
ElManu3le <msancho620@yahoo.es>
2021-02-08 20:19:38 +0100

    Practica3

---
+++ practica3/src/main/java/Practica3_AD/App.java
@@ -0,0 +1,13 @@
+package Practica3_AD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
